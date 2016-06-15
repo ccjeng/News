47070d7dc376a239ddc6caec787c9e29d8175bec
andycheng <ccjeng@gmail.com>
2016-06-15 13:04:32 +0800

    move api key tog gradle.properties

---
+++ app/src/main/java/com/ccjeng/news/utils/Constant.java
@@ -0,0 +1,14 @@
+package com.ccjeng.news.utils;
+
+import com.ccjeng.news.BuildConfig;
+
+/**
+ * Created by andycheng on 2016/2/26.
+ */
+public class Constant {
+    public static final String Ad_MoPub_Main = BuildConfig.Ad_MoPub_Main;
+    public static final String Ad_MoPub_Category = BuildConfig.Ad_MoPub_Category;
+    public static final String Ad_MoPub_RSS = BuildConfig.Ad_MoPub_RSS;
+    public static final String AD_MoPub_View= BuildConfig.AD_MoPub_View;
+
+}
